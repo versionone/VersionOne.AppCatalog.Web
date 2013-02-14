7de23436dd59603368acdfb692e79000f733f6d0
codeEmitter <greg.johnson@versionone.com>
2013-02-14 17:55:14 -0500

    Modify output format.

--- scripts/appcatalog.js
+++ scripts/appcatalog.js
@@ -10,7 +10,7 @@ $(function () {
             if (list == null || typeof (dataItems) === 'undefined' || dataItems.length == 0) return;
             $.each(dataItems,
                 function (item) {
-                    list.append('<p>' + this.Name + '</p>');
+                    list.append('<p>' + this.Name + ' - ' + this.Source + ' - ' + this.SourceUrl + '</p>');
                 });
             throw 'test';
         },
