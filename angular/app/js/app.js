3f67fa4fd27e8084d6885bfa09c44aae2524c6a9
Sean McCrohan <s.m.mccrohan@gmail.com>
2013-04-17 19:10:24 -0400

    Added AngularUI image carousel to details.

--- angular/app/js/app.js
+++ angular/app/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('appCatalog', ['appCatalog.filters', 'appCatalog.services', 'appCatalog.directives', 'appCatalog.controllers']).
+angular.module('appCatalog', ['appCatalog.filters', 'appCatalog.services', 'appCatalog.directives', 'appCatalog.controllers', 'ui.bootstrap']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {templateUrl: 'partials/list.html', controller: 'ListCtrl'});
     $routeProvider.when('/Details/:appId', {templateUrl: 'partials/details.html', controller: 'DetailsCtrl'});
