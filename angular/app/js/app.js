07a28f32cce1cee68ee357a769bca3936591e546
Sean McCrohan <s.m.mccrohan@gmail.com>
2013-04-18 18:18:36 -0400

    Added alternate layout (in progress)

--- angular/app/js/app.js
+++ angular/app/js/app.js
@@ -6,5 +6,6 @@ angular.module('appCatalog', ['appCatalog.filters', 'appCatalog.services', 'appC
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {templateUrl: 'partials/list.html', controller: 'ListCtrl'});
     $routeProvider.when('/Details/:appId', {templateUrl: 'partials/details.html', controller: 'DetailsCtrl'});
+    $routeProvider.when('/Details2/:appId', {templateUrl: 'partials/details2.html', controller: 'DetailsCtrl'});
     $routeProvider.otherwise({redirectTo: '/'});
   }]);
