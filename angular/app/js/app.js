9fc464918e2fe1be1f9f08cec3afe2b1bc6103cf
Sean McCrohan <s.m.mccrohan@gmail.com>
2013-04-22 13:05:08 -0400

    Moved old layout to Details-old, preserving for comparison. Moved new layout to Details.

--- angular/app/js/app.js
+++ angular/app/js/app.js
@@ -1,11 +1,10 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
 angular.module('appCatalog', ['appCatalog.filters', 'appCatalog.services', 'appCatalog.directives', 'appCatalog.controllers', 'ui.bootstrap']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {templateUrl: 'partials/list.html', controller: 'ListCtrl'});
+    $routeProvider.when('/DetailsOld/:appId', {templateUrl: 'partials/details-old.html', controller: 'DetailsCtrl'});
     $routeProvider.when('/Details/:appId', {templateUrl: 'partials/details.html', controller: 'DetailsCtrl'});
-    $routeProvider.when('/Details2/:appId', {templateUrl: 'partials/details2.html', controller: 'DetailsCtrl'});
     $routeProvider.otherwise({redirectTo: '/'});
   }]);
