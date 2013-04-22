3eb5e3f8ba14b4daacfbd66abf2bbee4e952d8c5
Sean McCrohan <s.m.mccrohan@gmail.com>
2013-04-22 17:42:41 -0400

    First pass at unit tests

--- angular/config/karma.conf.js
+++ angular/config/karma.conf.js
@@ -3,16 +3,17 @@ basePath = '../';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
+  'test/lib/jquery-1.8.1.min.js',
   'app/lib/angular/angular.js',
-  'app/lib/angular/angular-*.js',
   'test/lib/angular/angular-mocks.js',
+/*  'test/lib/angular/angular-scenario.js', */
   'app/js/**/*.js',
   'test/unit/**/*.js'
 ];
 
 autoWatch = true;
 
-browsers = ['Chrome'];
+browsers = ['C:/Program Files (x86)/Google/Chrome/Application/chrome.exe']
 
 junitReporter = {
   outputFile: 'test_out/unit.xml',
