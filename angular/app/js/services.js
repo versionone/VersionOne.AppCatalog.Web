962aac1ceb054cf8ca17007f6db95200fc2f6893
jogoshugh <jgough@JGough.corp.versionone.net>
2013-05-02 15:52:06 -0400

    Trigger deploy into staging

--- angular/app/js/services.js
+++ angular/app/js/services.js
@@ -4,6 +4,6 @@
 
 angular.module('appCatalog.services', ['ngResource']).
 	factory('App', function($resource) {
-		var App = $resource("http://appcatalog.azurewebsites.net/entry");
+		var App = $resource("http://appcatalogstage.azurewebsites.net/entry");
 		return App;
 	});
