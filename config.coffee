8f27ceb9b981adfbc1e7218dffdf3c20e1ee25e9
jogoshugh <jgough@JGough.corp.versionone.net>
2013-05-21 21:13:17 -0400

    Modified config names slightly and other good things

--- config.coffee
+++ config.coffee
@@ -4,16 +4,16 @@ config = {}
 
 nconf.file('config.json').env()
 
-config.entryRoute = nconf.get('server.entryRoute') || '/entry'
+config.entryRoute = nconf.get('server_entryRoute') || '/entry'
 
 console.log config.entryRoute
 
-config.port = nconf.get 'server.localPort'
+config.port = nconf.get('server_localPort') || 8081
 if process.env.PORT?
   config.port = process.env.PORT
 
-config.mongoUri = nconf.get 'mongoDb.uri'
-config.user = nconf.get 'server.auth.user'
-config.password = nconf.get 'server.auth.password'
+config.mongoUri = nconf.get 'mongoDb_uri'
+config.user = nconf.get 'server_auth_user'
+config.password = nconf.get 'server_auth_password'
 
 module.exports = config
--- config.js
+++ config.js
@@ -8,21 +8,21 @@
 
   nconf.file('config.json').env();
 
-  config.entryRoute = nconf.get('server.entryRoute') || '/entry';
+  config.entryRoute = nconf.get('server_entryRoute') || '/entry';
 
   console.log(config.entryRoute);
 
-  config.port = nconf.get('server.localPort');
+  config.port = nconf.get('server_localPort') || 8081;
 
   if (process.env.PORT != null) {
     config.port = process.env.PORT;
   }
 
-  config.mongoUri = nconf.get('mongoDb.uri');
+  config.mongoUri = nconf.get('mongoDb_uri');
 
-  config.user = nconf.get('server.auth.user');
+  config.user = nconf.get('server_auth_user');
 
-  config.password = nconf.get('server.auth.password');
+  config.password = nconf.get('server_auth_password');
 
   module.exports = config;
 
