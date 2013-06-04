2b014c066347cd2337f2f7013ce640c913ea5366
jogoshugh <jgough@JGough.corp.versionone.net>
2013-06-04 14:05:42 -0400

    Fixing iisnode

--- src/scripts/updateCatalog.sh
+++ src/scripts/updateCatalog.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
-if [[ -n $(git diff-tree --no-commit-id --name-only -r HEAD |grep changelog.coffee) ]]; then
-	echo curl -X PUT http://appcatalog.azurewebsites.net/entry -d @changelog.json -H "Content-type: application/json"
+#!/bin/bash
+if [[ -n $(git diff-tree --no-commit-id --name-only -r HEAD |grep product.json) ]]; then
+	echo curl -X PUT http://appcatalogstage.azurewebsites.net/entry --user catUser:CatsAreUs -d @product.json -H "Content-type: application/json"
 else
-	echo It did not change..
+	echo No change to product.json detected, skipping App Catalog update
 fi
 
