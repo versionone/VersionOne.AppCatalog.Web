95c7be15724e44f3f61ebb5480d0a4772358dcb3
jogoshugh <jgough@JGough.corp.versionone.net>
2013-06-05 22:28:17 -0400

    fixed path to new site

--- src/scripts/copyProduct.sh
+++ src/scripts/copyProduct.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo Copying http://appcatalogstage.azurewebsites.net/entry?id=$1 to product.json
 curl http://appcatalogstage.azurewebsites.net/entry/?id=$1 >product.json && \
-curl -X PUT http://appcatalog.azurewebsites.net/entry --user $2 -d @product.json -H "Content-Type: application/json"
+curl -X PUT http://v1appcatalog.azurewebsites.net/entry --user $2 -d @product.json -H "Content-Type: application/json"
