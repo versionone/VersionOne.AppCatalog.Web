7c85b4673fab4fb44158922f833d196e2d2e8595
Sean McCrohan <s.m.mccrohan@gmail.com>
2013-06-11 15:03:52 -0400

    Install last module, which isn't available in npm

--- src/client/scripts/karma-setup.sh
+++ src/client/scripts/karma-setup.sh
@@ -5,3 +5,6 @@ npm install karma-coverage
 npm install karma-phantomJS-launcher
 npm install karma-junit-reporter
 npm install PhantomJS
+
+cd ~/AppData/Roaming/npm/node_modules
+git clone https://github.com/karma-runner/karma-ng-html2js-preprocessor.git
